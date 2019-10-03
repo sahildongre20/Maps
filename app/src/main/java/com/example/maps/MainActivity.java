1aa4c8a03f0634ff4111930c42a9a74937864ed0
sahildongre20 <54145208+sahildongre20@users.noreply.github.com>
2019-10-03 23:22:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment  mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        LatLng MAHARASHTRA = new LatLng(19.017656,72.856178);
+            map.addMarker(new MarkerOptions().position(MAHARASHTRA).title("MAHARASHTRA"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(MAHARASHTRA));
+
+
+
+    }
+}
